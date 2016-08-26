0ed4a422ae196cdfbb868db5cecbe2269bc83a7b
kevin olson <acidjazz@gmail.com>
2016-08-26 00:51:37 -0700

    i forgot

--- scripts/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-s3cmd --config=/Users/k/.s3definery sync public/ s3://gilmanbrewing.com --exclude '.DS_Store'
-s3cmd setacl --config=/Users/k/.s3definery s3://gilmanbrewing.com/ --acl-public --recursive
